6031095c29eb2a5d81f51151b8196996f718dcf3
Maik Riechert <maik.riechert@arcor.de>
2014-09-06 16:54:33 +0200

    some progress on compiling lensfun

--- setup.py
+++ setup.py
@@ -1,8 +1,47 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import numpy
 import subprocess
 import errno
 import os
+import sys
+import urllib
+import zipfile
+
+isWindows = os.name == 'nt'
+is64Bit = sys.maxsize > 2**32
+
+if isWindows:
+    # download glib2 dlls and dev package and extract into external/lensfun/glib-2.0
+    glib_arch = 'win64' if is64Bit else 'win32'
+    glib_libs_url = 'http://win32builder.gnome.org/packages/3.6/glib_2.34.3-1_{}.zip'.format(glib_arch)
+    glib_dev_url = 'http://win32builder.gnome.org/packages/3.6/glib-dev_2.34.3-1_{}.zip'.format(glib_arch)
+    glib_files = [(glib_libs_url, 'glib_2.34.3-1.zip'), (glib_dev_url, 'glib-dev_2.34.3-1.zip')]
+    for url, path in glib_files:
+        if os.path.exists(path):
+            break
+        print 'Downloading {}'.format(url)
+        urllib.urlretrieve(url, path)
+        with zipfile.ZipFile(path) as z:
+            z.extractall('external/lensfun/glib-2.0')
+        
+    # configure and compile lensfun, we need the .dll and lensfun.h
+    # lensfun requires GNU Make and glib2
+    cwd = os.getcwd()
+    os.chdir('external/lensfun')
+    conf_arch = 'x86_64' if is64Bit else 'x86'
+    
+    # FIXME configure doesn't find glib-2.0 
+    cmds = ['python configure --compiler=msvc --target=windows.' + conf_arch + ' --mode=release ' +\
+            '--prefix= --bindir= --includedir= --libdir= --docdir= --datadir= --sysconfdir= --libexecdir=',
+            'make libs'
+            ]
+    for cmd in cmds:
+        print cmd
+        if os.system(cmd) != 0:
+            sys.exit()   
+    os.chdir(cwd)
+    
+    # TODO check if the files that we need were produced     
 
 # adapted from cffi's setup.py
 # the following may be overridden if pkg-config exists
@@ -47,7 +86,13 @@ def use_pkg_config():
     _ask_pkg_config(extra_link_args,    '--libs-only-other')
     _ask_pkg_config(libraries,          '--libs-only-l', '-l')
 
-use_pkg_config()
+if isWindows:
+    include_dirs += ['external/stdint', 
+                     'external/lensfun/include/lensfun']
+    #library_dirs += ['external/lensfun/glib-2.0/bin']
+    # TODO continue
+else:
+    use_pkg_config()
 
 include_dirs += [numpy.get_include()]
 
