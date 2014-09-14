ca5c8f96912caae39e57b9a8e38c8b7abc0a0363
Maik Riechert <maik.riechert@arcor.de>
2014-09-14 11:46:05 +0200

    I don't get it...

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,4 +1,6 @@
 $gitinfo = `git log -n 1 --pretty=format:'%d'`
+echo "APPVEYOR_REPO_BRANCH: $env:APPVEYOR_REPO_BRANCH"
+echo "git log: $gitinfo"
 if (($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($gitinfo -match "tag: v")) {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Out-File $env:userprofile\.pypirc
   $env:CMD_IN_ENV python setup.py bdist_wheel upload
