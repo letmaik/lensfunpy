95afae45ff2e7e28d0525add11f498c6572cea44
Maik Riechert <maik.riechert@arcor.de>
2014-09-14 12:28:57 +0200

    test

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,6 +1,9 @@
+git log -n 1 --pretty=format:'%d'
 $gitinfo = `git log -n 1 --pretty=format:'%d'`
+dir env:
 echo "APPVEYOR_REPO_BRANCH: $env:APPVEYOR_REPO_BRANCH"
 echo "git log: $gitinfo"
+exit
 if (($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($gitinfo -match "tag: v")) {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Out-File $env:userprofile\.pypirc
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
