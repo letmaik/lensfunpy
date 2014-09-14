ebcb870ece676feff83b4ed60402143823ea81aa
Maik Riechert <maik.riechert@arcor.de>
2014-09-14 12:53:12 +0200

    powershell is complicated

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,10 +1,8 @@
-git log -n 1 --pretty=format:'%d'
+#requires -version 2.0
+
 $gitinfo = `git log -n 1 --pretty=format:'%d'`
-dir env:
-echo "APPVEYOR_REPO_BRANCH: $env:APPVEYOR_REPO_BRANCH"
 echo "git log: $gitinfo"
-exit
-if (($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($gitinfo -match "tag: v")) {
+if (($gitinfo -match "master") -and ($gitinfo -match "tag: v")) {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Out-File $env:userprofile\.pypirc
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
 }
